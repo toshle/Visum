e96531c3d7b0dc30ec0172640500b248c353c621
toshle <rosen_ra4ev@abv.bg>
2014-06-15 21:21:52 +0300

    Add tab buttons

---
+++ app/view/GMap.js
@@ -0,0 +1,29 @@
+Ext.define("Visum.view.GMap", {
+    extend : "Ext.Map",
+    xtype  : 'myMap',
+    config : {
+        scroll: false,
+        title              : 'Map',
+        iconCls            : 'maps',
+        useCurrentLocation : true,
+        mapOptions         : {
+            zoom              : 16,
+            marker            : true,
+            navigationControl : false
+        },
+        listeners          : {
+            maprender : function(comp, map) {
+                var geo = comp.getGeo();
+
+                new google.maps.Marker({
+                    map       : this.getMap(),
+                    position  : new google.maps.LatLng(geo.getLatitude(), geo.getLongitude()),
+                    title     : 'Drag Marker To New Position',
+                    animation : google.maps.Animation.DROP,
+                    draggable : true
+                });
+            }
+        }
+
+    }
+});
---
+++ app/view/Main.js
@@ -0,0 +1,65 @@
+Ext.define('Visum.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [{
+            title: 'Home',
+            iconCls: 'home',
+
+            styleHtmlContent: true,
+            scrollable: true,
+
+            items: {
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'Home tab'
+            },
+
+            html: [
+                "Welcome to Visum."
+            ].join("")
+        }, {
+            xtype: 'myMap',
+
+            items: [{
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'Map'
+            }]
+        }, {
+            title: 'Aug Reality',
+            iconCls: 'eye',
+
+            items: [{
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'Augmented Reality'
+            }, {
+                styleHtmlContent: true,
+                html: [
+                    "Aug Reality"
+                ].join("")
+            }]
+        }, {
+            title: 'Profile',
+            iconCls: 'user',
+
+            items: [{
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'User Profile'
+            }, {
+                styleHtmlContent: true,
+                html: [
+                    "Welcome to your profile"
+                ].join("")
+            }]
+        }]
+    }
+});
