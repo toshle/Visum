aeb12c15d6222659a000b3e25f85b82872c6b677
toshle <rosen_ra4ev@abv.bg>
2014-06-23 01:19:23 +0300

    Modify Home view

--- app/view/Home.js
+++ app/view/Home.js
@@ -1,14 +1,62 @@
 Ext.define('Visum.view.Home', {
     extend: 'Ext.Container',
     xtype: 'home',
+    requires: [
+        'Ext.dataview.List'
+    ],
     config: {
+        title: 'Home',
+        styleHtmlContent: true,
+        layout: 'vbox',
         items: [{
-            title: 'Home',
-            styleHtmlContent: true,
-            html: [
-                "Home PAAAAAAAAAAAGE<br>",
-                "So HARDCORE!....>!"
-            ].join("")
-        }]
+                xtype: 'panel',
+                layout: 'hbox',
+                defaults: {
+                    xtype: 'panel',
+                    flex: 1,
+                    minHeight: '300px',
+                    margin: '5 5 5 5'
+                },
+                items: [{
+                    items: [{
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Latest Points of Interest'
+                    }, {
+                        xtype: 'list',
+                        minHeight: '260px',
+                        data: [{
+                            name: 'Fantastico - Верен приятел'
+                        }, {
+                            name: 'Национален Дворец на Културата - НДК'
+                        }],
+                        itemTpl: new Ext.XTemplate('{name}')
+                    }]
+                }, {
+                    items: [{
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'News'
+                    }, {
+                        xtype: 'list',
+                        minHeight: '260px',
+                        data: [{
+                            name: 'Extremely interesting news straight from the source.'
+                        }, {
+                            name: 'Doge has visited Sofia! Such news! MUCH information...'
+                        }],
+                        itemTpl: new Ext.XTemplate('{name}')
+                    }]
+                }]
+            }, {
+                xtype: 'panel',
+                html: [
+                    'WoW<br>',
+                    'Is this? REAL?'
+                ].join()
+
+            }
+
+        ]
     }
 });
